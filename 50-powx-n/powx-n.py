70f87e1d7c20a432015feada60df23455af3369a
Udaysai18 <udaysaianichinni@gmail.com>
2026-09-22 21:50:42 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (28.77%) - LeetSync

---
+++ 50-powx-n/powx-n.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def myPow(self, x, n):
+        import math
+        return math.pow(x,n)
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        
